be64eb4d278df09b47e7952d683ebb4fc4e0e554
devleandrodias <leandrodbdias@gmail.com>
2022-08-15 22:55:21 -0300

    refactor: rename variable name to header

--- challenges/react/01-todo-list/src/components/Header/index.tsx
+++ challenges/react/01-todo-list/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import rocketLogo from '../../assets/rocket.svg'
 import styles from './Header.module.css';
 
 export const Header = () => {
-  return <div className={styles.headerContainer}>
+  return <div className={styles.header}>
     <img src={rocketLogo} />
   </div>
 }
