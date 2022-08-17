e257f3df5296ad115fb06d12ac2d74484673c1fd
devleandrodias <leandrodbdias@gmail.com>
2022-08-16 21:30:15 -0700

    feat: finish first version of todo list challenge

--- challenges/react/01-todo-list/src/components/TodoList/index.tsx
+++ challenges/react/01-todo-list/src/components/TodoList/index.tsx
@@ -1,43 +1,133 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import { useForm } from "react-hook-form";
 
-import { Bookmarks, Circle, PlusCircle, Trash } from "phosphor-react";
+import {
+  Trash,
+  Circle,
+  Bookmarks,
+  PlusCircle,
+  CircleWavyCheck,
+} from "phosphor-react";
 
 import styles from "./TodoList.module.css";
 
+type AddNewTodoForm = {
+  todo: string;
+};
+
+type Todo = {
+  id: string;
+  description: string;
+  status: "done" | "undone";
+};
+
 export const TodoList = () => {
-  const [tasks, setTasks] = useState<string[]>([]);
+  const { register, handleSubmit, reset } = useForm<AddNewTodoForm>({
+    defaultValues: { todo: "" },
+  });
 
-  const existsTasks = tasks.length > 0;
-  const countTasksCreated = tasks.length;
+  const [todoList, setTodoList] = useState<Todo[]>([]);
 
-  const handleAddNewTask = function () {
-    setTasks([...tasks, "Create todo list challenge"]);
+  const existsTodos = todoList.length > 0;
+  const countTotalTodos = todoList.length;
+
+  const countTotalDoneTodos = todoList.reduce((prev, current) => {
+    if (current.status === "done") prev++;
+    return prev;
+  }, 0);
+
+  const handleMarkTodoAsDone = function (todoId: string) {
+    const updatedList = todoList.map((todo) => {
+      if (todo.id === todoId) {
+        todo.status = "done";
+      }
+
+      return todo;
+    });
+
+    setTodoList(updatedList);
+  };
+
+  const handleMarkTodoAsUndone = function (todoId: string) {
+    const updatedList = todoList.map((todo) => {
+      if (todo.id === todoId) {
+        todo.status = "undone";
+      }
+
+      return todo;
+    });
+
+    setTodoList(updatedList);
+  };
+
+  const handleRemoveTodoById = function (todoId: string) {
+    const filteredTodos = todoList.filter(({ id }) => id !== todoId);
+    setTodoList(filteredTodos);
+  };
+
+  const handleAddNewTodo = function ({ todo }: AddNewTodoForm) {
+    const newTodo: Todo = {
+      id: uuid(),
+      description: todo,
+      status: "undone",
+    };
+    setTodoList([...todoList, newTodo]);
+    reset();
   };
 
   return (
     <div className={styles.todoList}>
-      <div className={styles.searchContainer}>
-        <input placeholder="Adicione uma nova tarefa" />
-        <button onClick={handleAddNewTask}>
-          Criar
-          <PlusCircle />
-        </button>
-      </div>
+      <form onSubmit={handleSubmit(handleAddNewTodo)}>
+        <div className={styles.searchContainer}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            {...register("todo")}
+          />
+          <button>
+            Criar
+            <PlusCircle />
+          </button>
+        </div>
+      </form>
       <div className={styles.taskHeader}>
         <span>
           Tarefas criadas
-          <span className={styles.counter}>{countTasksCreated}</span>
+          <span className={styles.counter}>{countTotalTodos}</span>
         </span>
         <span>
-          Concluidas<span className={styles.counter}>0</span>
+          Concluidas
+          <span className={styles.counter2}>
+            {countTotalDoneTodos} de {countTotalTodos}
+          </span>
         </span>
       </div>
-      {existsTasks ? (
-        tasks.map((task) => (
-          <div className={styles.taskCard}>
-            <Circle size={16} />
-            <span>{task}</span>
-            <Trash size={16} className={styles.iconTrash} />
+      {existsTodos ? (
+        todoList.map(({ id, description, status }) => (
+          <div
+            key={id}
+            className={
+              status === "done" ? styles.taskCardDone : styles.taskCard
+            }
+          >
+            {/* Melhorar essa IF para aplicar um botao ou outro */}
+            {status === "done" ? (
+              <CircleWavyCheck
+                size={16}
+                weight="fill"
+                color="#1e6f9f"
+                onClick={() => handleMarkTodoAsUndone(id)}
+              />
+            ) : (
+              <Circle size={16} onClick={() => handleMarkTodoAsDone(id)} />
+            )}
+            <span>{description}</span>
+            <Trash
+              size={16}
+              className={styles.iconTrash}
+              onClick={() => handleRemoveTodoById(id)}
+            />
           </div>
         ))
       ) : (
