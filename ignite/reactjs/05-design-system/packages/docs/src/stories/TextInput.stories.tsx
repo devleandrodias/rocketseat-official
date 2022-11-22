7e3f17bad98739b3bffe4bc5deccd1bb69bcef71
devleandrodias <leandrodbdias@gmail.com>
2022-11-21 21:17:23 -0300

    feat: styling text input component

---
+++ ignite/reactjs/05-design-system/packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,36 @@
+import type { StoryObj, Meta } from "@storybook/react";
+
+import { Box, Text, TextInput, TextInputProps } from "@devleandrodias/react";
+
+const textConfig: Meta<TextInputProps> = {
+	title: "Form/Texrt Input",
+	component: TextInput,
+	args: {},
+	decorators: [
+		(story) => {
+			return (
+				<Box
+					as="label"
+					css={{ display: "flex", flexDirection: "column", gap: "$2" }}
+				>
+					<Text size="sm">Email address</Text>
+					{story()}
+				</Box>
+			);
+		},
+	],
+};
+
+export default textConfig;
+
+export const Primary: StoryObj<TextInputProps> = {
+	args: { placeholder: "Type your name" },
+};
+
+export const Disabled: StoryObj<TextInputProps> = {
+	args: { disabled: true },
+};
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+	args: { prefix: "call.com/" },
+};
