a663fe9667836458d79f9a6fe2842903d66280a7
devleandrodias <leandrodbdias@gmail.com>
2023-04-07 10:38:14 -0300

    feat: start v2 node fundamentals

---
+++ ignite/nodejs/v2-node-fundamentals/request.sh
@@ -0,0 +1,8 @@
+# Create a user
+# curl -X POST http://localhost:4000/users
+
+# Get a user list
+# curl -X GET http://localhost:4000/users
+
+# URL Not Found
+curl http://localhost:4000/
--- ignite/nodejs/v2-node-fundamentals/src/server.js
+++ ignite/nodejs/v2-node-fundamentals/src/server.js
@@ -1,7 +1,38 @@
 import http from 'node:http'
 
+/**
+ * HTTP (Hypertext Transfer Protocol)
+ * 
+ * - METHOD AND URL (GET /user)
+ * - GET, POST, PUT, PATCH, DELETE
+ * - Stateful X Stateless
+ * - JSON (Javascript Object Notation)
+ * - Headers (Request, Response) === Metadatas
+ * - Http Status Code
+ */
+
+const users = []
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello Ignite Up!')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@gmail.com'
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(4000)
